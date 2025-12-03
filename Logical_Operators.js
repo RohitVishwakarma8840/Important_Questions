9667d074b023dae8bda6f2b52827f30ac570e248
Rohit Vishwakarma <128726730+RohitVishwakarma8840@users.noreply.github.com>
2025-12-03 19:22:02 +0530

    Add logical operators demonstration in Logical_Operators.js

---
+++ Logical_Operators.js
@@ -0,0 +1,22 @@
+const result = true && false;    // false
+const result2 = false && false;  // false
+const result3 = true && true;   // true
+const result4 = false && true;  // false
+
+console.log(result);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+
+console.log(" ");
+console.log(" ");
+
+const result8 = true || false;     // true
+const result5 = false || false;    // false
+const result6 = true || true;      // true
+const result7 = false || true;     // true
+
+console.log(result8);
+console.log(result5);
+console.log(result6);
+console.log(result7);
